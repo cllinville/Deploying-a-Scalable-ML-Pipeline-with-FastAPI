ff911bfb6dd6b2e7b4060fe74dcb973608b20c03
cllinville <ceiaralinville@gmail.com>
2026-02-11 14:12:13 -0500

    Complete API local client with screenshot

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
-
+import os
 import pandas as pd
-
+from fastapi import FastAPI
 from pydantic import BaseModel, Field
 from ml.model import load_model
 from ml.data import apply_label, process_data
@@ -28,40 +28,59 @@ class Data(BaseModel):
     native_country: str = Field(..., example="United-States", alias="native-country")
 
 
-path = None  # TODO: enter the path for the saved encoder
-encoder = load_model(path)
+# create a RESTful API using FastAPI
+app = FastAPI()
+app.state.encoder = None
+app.state.model = None
 
-path = None  # TODO: enter the path for the saved model
-model = load_model(path)
+@app.on_event("startup")
+def startup_event():
+    encoder_path = os.path.join("model", "encoder.pkl")
+    model_path = os.path.join("model", "model.pkl")
 
-# TODO: create a RESTful API using FastAPI
-app = None  # your code here
+    app.state.encoder = load_model(encoder_path)
+    app.state.model = load_model(model_path)
 
+    print("Loaded encoder:", app.state.encoder)
+    print("Loaded model:", app.state.model)
 
-# TODO: create a GET on the root giving a welcome message
+# create a GET on the root giving a welcome message
 @app.get("/")
 async def get_root():
     """Say hello!"""
-    # your code here
-    pass
+    return {"message": "Hello from the API!"}
 
 
-# TODO: create a POST on a different path that does model inference
+#create a POST on a different path that does model inference
 @app.post("/data/")
 async def post_inference(data: Data):
     # DO NOT MODIFY: turn the Pydantic model into a dict.
-    data_dict = data.dict()
+    data_dict = data.dict(by_alias=True)
     # DO NOT MODIFY: clean up the dict to turn it into a Pandas DataFrame.
     # The data has names with hyphens and Python does not allow those as variable names.
     # Here it uses the functionality of FastAPI/Pydantic/etc to deal with this.
     data = {k.replace("_", "-"): [v] for k, v in data_dict.items()}
     data = pd.DataFrame.from_dict(data)
+    encoder = app.state.encoder
+    model = app.state.model
+    cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
 
     data_processed, _, _, _ = process_data(
-        # your code here
-        # use data as data input
-        # use training = False
-        # do not need to pass lb as input
+        data,
+        categorical_features=cat_features,
+        label=None,
+        training=False,
+        encoder=encoder,
+        lb=None,
     )
-    _inference = None  # your code here to predict the result using data_processed
+    _inference = model.predict(data_processed)  # predict the result using data_processed
     return {"result": apply_label(_inference)}
